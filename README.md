76d500da7eb2aacd6f6a24c9316692739cca26d9
cfop478 <rogercampsviader@gmail.com>
2024-01-24 10:37:32 +0100

    Readme writing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pong-Game
 Pong game using Turtle Python
+1. Create two paddles A and B on the left and right side of the screen.
+2. Create a ball.
+3. Create an event to move the paddle vertically on pressing a certain key.
+4. Create the function to update the score after each player misses a collision.
--- pong.py
+++ pong.py
@@ -125,8 +125,8 @@ while True:
 	# Paddle ball collision
 	if (hit_ball.xcor() > 360 and hit_ball.xcor() < 370) and (hit_ball.ycor() < right_pad.ycor()+40 and hit_ball.ycor() > right_pad.ycor()-40):
 		hit_ball.setx(360)
-		hit_ball.dx*=-1
+		hit_ball.dx*= -1
 		
 	if (hit_ball.xcor()<-360 and hit_ball.xcor()>-370) and (hit_ball.ycor()<left_pad.ycor()+40 and hit_ball.ycor()>left_pad.ycor()-40):
 		hit_ball.setx(-360)
-		hit_ball.dx*=-1
+		hit_ball.dx*= -1
